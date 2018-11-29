cf9e4dc7451a433d6889c4e71cc12a9013320f50
Martina Fernandez <martinalucia.fdez@gmail.com>
2018-11-29 16:07:06 +0000

    commencing git for python module 2 project

---
+++ ch01/ch01-martina.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+A =2 + 3
+B = 2.9+3.2
+C = (3.2/2.0) + 7
+D = 2**3
+E = 4**(1/2)
+F = str(6)+'a'
+
+print(A)
+print(B)
