2b78e3b4685db57933ee719f92d13afff1b330ea
Martina Fernandez <martinalucia.fdez@gmail.com>
2018-12-19 10:34:44 +0000

    5-7 chapters

--- ch05/ch05-Argvs.py
+++ ch05/ch05-Argvs.py
@@ -6,76 +6,9 @@ Created on Thu Dec  6 11:23:03 2018
 """
 #further class practice with real life applications
 
-#class Customer(object):
-#    """A customer of ABC Bank with a checking account. Customers have the following properties:
-#    Attributes:
-#    name: A string representing the customer's name.
-#    balance: A float tracking the current balance of the customer's account.
-#    """
-#    def __init__(self, name, balance=0.0):
-#        """Return a Customer object whose name is *name* and starting balance is *balance*."""
-#        self.name = name
-#        self.balance = balance
-#    def withdraw(self, amount):
-#        """Return the balance remaining after withdrawing *amount* dollars."""
-#        if amount > self.balance:
-#            raise RuntimeError('Amount greater than available balance.')
-#        self.balance -= amount
-#        return self.balance
-#    def deposit(self, amount):
-#        """Return the balance remaining after depositing *amount* dollars."""
-#        self.balance += amount
-#        return self.balance
-#    
-#    
-#cust1 = Customer('martina fernandez', 1000.0)    
+ 
 #    
 
-#
-#
-#
-#
-#
-#class Animal():
-#    def __init__(self, legs, colour):
-#        self.legs = legs
-#        self.colour = colour
-#
-#    def eat(self):
-#        print('yum')
-#        
-#class Dog(Animal):
-#    def bark(self):
-#        print('woof')
-        
-
-        
-        
-        
-#        
-#class Cat(Animal):
-#    def meow(self):
-#        print('meow')
-
-#
-#
-#snoopy = Dog(4, "pink")
-#snoopy.bark()
-#snoopy.eat()
-#
-#garfield = Cat(4, "brown")
-#garfield.meow()
-#garfield.eat()
-
-
-#        
-#class aquarium():
-#    def __init__(self, name, legs):
-#        self.name=name
-#        self.legs=legs
-#        
-#fish1=aquarium('octopus', 8)
-#fish2=aquarium('shark' ,0)
 
 
 
--- ch05/foo.py
+++ ch05/foo.py
@@ -3,53 +3,6 @@
 Created on Thu Dec  6 13:46:26 2018
 
 @author: mluci
-"""
-#practice with argv for practicing with the command line
-import sys
-print(sys.argv) #argv returns a list of al the argumetns in your programme
-
-
-#import sys
-#
-#if len(sys.argv) < 2:
-#    print("what is your name")
-#    sys.exit()
-#    
-#print("Hello, " + sys.argv[1])
-
-
-##argv LPTHW
-from sys import argv
-
-script, filename = argv
-
-print ("We're going to erase {}.".format(filename))
-print ("If you don't want that, hit CTRL- C (^C).")
-print ("If you do want that, hit RETURN.")
-raw_input("?")
-print ("Opening the file...")
-target = open(filename, 'w')
-print ("Truncating the file. Goodbye!")
-target.truncate()
-
-print ("Now I'm going to ask you for three lines.")
-line1 = raw_input("line 1: ")
-line2 = raw_input("line 2: ")
-line3 = raw_input("line 3: ")
-print ("I'm going to write these to the file.")
-
-
-target.write(line1)
-target.write("\n")
-
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
-
-print ("And finally, we close it.")
-target.close()
-
 
 
 
