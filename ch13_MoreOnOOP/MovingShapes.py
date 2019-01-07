c0d8df65b127f8968aaa81eb2b614359e102d532
Martina Fernandez <martinalucia.fdez@gmail.com>
2019-01-07 10:33:32 +0000

    adding homework

--- ch13_MoreOnOOP/MovingShapes.py
+++ ch13_MoreOnOOP/MovingShapes.py
@@ -10,24 +10,57 @@ from Shapes import *
 from pylab import random as r
 
 class MovingShape:
-    def __init__(self, frame, shape, diameter, x=(0,0), y=(0,0), dx=10, dy=10):
+    def __init__(self, frame, shape, diameter):
         self.shape = shape
         self.diameter = diameter
         self.figure = Shape(shape, diameter)
-        self.x= x
-        self.y= y
-        self.dx=dx
-        self.dy=dy
+        
+        #Maximum and minimum start positions#
+        self.minx = self.diameter / 2
+        self.miny = self.diameter / 2
+        self.maxx = frame.width - self.diameter / 2
+        self.maxy = frame.height - self.diameter / 2
+        
+        #Adding random variation for start positions#
+        self.x = self.minx + r() * (self.maxx - self.minx)
+        self.y = self.miny + r() * (self.maxy - self.miny)
+        
+        #Create random movement#
+        self.dx = 5 + 10 * r() 
+        self.dy = 5 + 10 * r()
+        
+
+        
+#        #Move in positive and negative directions#
+#        if r() < 0.5:
+#            self.dx = 5 + 10 * r() * -1
+#            self.dy = 5 + 10 * r() * -1
+#        else:
+#            self.dx = 5 + 10 * r()
+#            self.dy = 5 + 10 * r()
+            
     def goto(self, x, y):
         self.figure.goto(x,y)
-    def moveTick(self,x, y):
+        
+        
+    def moveTick(self): 
         self.x= self.x + self.dx
         self.y= self.y + self.dy
-        self.figure.goto(x,y)
+        self.figure.goto(self.x,self.y)
+
         
+        #Hitting the wall#
+        if self.x <= self.minx:
+            self.dx = self.dx * -1
+        if self.y <= self.miny:
+            self.dy = self.dy * -1
+        if self.x >= self.maxx:
+            self.dx = self.dx * -1
+        if self.y >= self.maxy:
+            self.dy = self.dy * -1
+            
+
         
-        
- #       pass
     
 class Square(MovingShape):
     def __init__(self, frame, diameter):
@@ -36,6 +69,13 @@ class Square(MovingShape):
 class Diamond(MovingShape):
     def __init__(self, frame, diameter):
         MovingShape.__init__(self, frame, 'diamond', diameter)
+        #Diamonds Vs Squares#
+        self.diameter =2 *self.diameter
+        self.minx = self.diameter
+        self.miny = self.diameter
+        self.maxx = frame.width - self.diameter / 2
+        self.maxy = frame.height - self.diameter / 2
+        
         
 class Circle(MovingShape):
     def __init__(self, frame, diameter):
--- ch13_MoreOnOOP/test_multi_shapes.py
+++ ch13_MoreOnOOP/test_multi_shapes.py
@@ -9,11 +9,18 @@ Created on Fri Dec 21 14:04:03 2018
 ##07/06: Adding tandom variation - velocities##
 
 from MovingShapes import *
-frame=Frame()
-numshapes=3
-shapes=[]
+
+frame = Frame()
+numshapes = 3
+shapes = []
+size = 60
 for i in range(numshapes):
-    shapes.append(Square(frame,100))
-for i in range(100):
+    shapes.append(Square(frame,size))
+    shapes.append(Diamond(frame,size))
+    shapes.append(Circle(frame,size))
+    
+for i in range(300):
     for shape in shapes:
         shape.moveTick()
+        
+frame.close()
